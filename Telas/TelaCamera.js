d5510b0adec42812f1d5f682258d2672431eb858
MrRestone <164062285+MrRestone@users.noreply.github.com>
2024-05-29 21:29:15 -0300

    0.0.7

--- Telas/TelaCamera.js
+++ Telas/TelaCamera.js
@@ -1,89 +1,94 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { CameraView } from 'expo-camera';
-import Header from '../Componentes/Header';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-function TelaCamera({navigation, route}) {
+export default function App() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [currentZoom, setCurrentZoom] = useState(0);
 
-    const [hasPermission, setHasPermission] = useState(null)
-    const [camera, setCamera] = useState(null);
-    const [Image, setImage] = useState(null);
+  if (!permission) {
+    return <View />;
+  }
 
-    useEffect(() => {
-        (async () => {
-            const{ status } = await CameraView.requestCameraPermissionsAsync();
-            setHasPermission(status === 'granted')
-        }) ();
-    },[]);
-
-    const takePicture = async () => {
-        if(camera){
-            const data = await camera.tekePictureAsync(null)
-            setImage(data)
-        }
-    }
-    if(!hasPermission){
-        return(
-            <View style={styles.container}>
-                <Text>Permissão da câmera negado!</Text>
-            </View>
-        )
-    }
+  if (!permission.granted) {
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                    <Header showNav={false}/>
-                </View>
-                    {Image ? (
-                      <View style={styles.container}>  
-                        <Image source={{ uri: Image.uri}} Style={styles.image}/>
-                        <Button
-                            color='gray'
-                            title="Tirar nova foto"
-                            onPress={() => {
-                             setImage(null)
-                            }}
-                        />    
-                        <Button 
-                        title="user essa foto"
-                        onPress={() => {
-                            navigation.navigate('addPost', {uid: route.params.uid, image: image.uri})
-                            }}
-                       /> 
-                       </View>
-                    ) : (
-                    <View style={styles.container}>
-                        <CameraView
-                            ref={(ref) => setCamera(ref)}
-                            style={styles.camera}
-                            type={type}
-                        >  
-                            <View style={styles.BotoesCamera}>
-                                <Pressble onPress={() => {switchCamera()}}>
-                                    <Image source={require('../assets/changeCamera.png')} resizeMode="contain" />
-                                </Pressble>  
-                                <Pressble onPress= {() =>{takePicture()}}>
-                                    <Image source={require('../assets/camera.png')} resizeMode ="contain"/>
-                                </Pressble>
-                            </View>
-                        </CameraView>
-                    </View>
-                    )}
-                </View>
-            )}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        aspectRatio: 1,
-    },
-    cameraContainer: {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    image: {
-        flex: 1,
-        resizeMode: 'contain'
-    }
-})
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
 
-export default TelaCamera 
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      let photo = await cameraRef.current.takePictureAsync({ flash: 'on' });
+      console.log(photo);
+    }
+  };
+
+  const zoomIn = () => {
+    setCurrentZoom(currentZoom + 0.1);
+  };
+
+  const zoomOut = () => {
+    setCurrentZoom(currentZoom - 0.1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing} zoom={currentZoom} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={zoomIn}>
+            <Text style={styles.text}>Zoom In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={zoomOut}>
+            <Text style={styles.text}>Zoom Out</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 3,
+    right: 0,
+  },
+  button: {
+    backgroundColor: 'green',
+    borderRadius: 8,
+    padding: 15,
+    paddingHorizontal: 6,
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
